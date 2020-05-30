0792ae67fb421722c2bccb49f539288a0f04c1ea
John L <51428041+TooManyJohns@users.noreply.github.com>
2020-05-30 01:54:27 -0400

    Re-did react navigation header to not include gestures enabled or a headerleft appearing. Keeping it for styling purposes

--- RNProject/App.js
+++ RNProject/App.js
@@ -7,8 +7,24 @@ import PokemonList from 'components/PokemonList'
 import HomePage from 'components/HomePage'
 
 export const RNProject =  StackNavigator({
-  Home: { screen: HomePage },
-  Info: { screen: PokemonList }
+  Home: { 
+    screen: HomePage 
+  },
+  Info: { 
+    screen: PokemonList,
+    navigationOptions:  {
+      headerLeft: null,
+      gesturesEnabled: false,
+  }
+  }
+}, 
+{
+  navigationOptions: 
+  {
+    headerStyle: {
+      backgroundColor: '#282828',
+    },
+  }
 })
 
 export default RNProject;
