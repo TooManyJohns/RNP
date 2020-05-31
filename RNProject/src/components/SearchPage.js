7802aeeda15a06f01b12ca6a0beb80de31a0c3b8
John L <51428041+TooManyJohns@users.noreply.github.com>
2020-05-30 21:51:00 -0400

    SearchPage asset folder, Background Styling for the SearchPage

--- RNProject/src/components/SearchPage.js
+++ RNProject/src/components/SearchPage.js
@@ -1,18 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import {View} from 'react-native'
+import { View, ImageBackground } from "react-native";
 
 import sty_SearchPage from "styles/SearchPageStyle";
 
+import img_topScrBackground from "assets/search/topScrSearch.png";
+import img_btmScrBackground from "assets/search/btmScrSearch.png";
+
 export default class SearchPage extends Component {
-    render() {
-        return (
-            <View style={sty_SearchPage.searchPageCtn}>
-                <View style={sty_SearchPage.topScreenCtn}>
-                </View>
-                <View style={sty_SearchPage.bottomScreenCtn}>
-                </View>
+  render() {
+    return (
+      <View style={sty_SearchPage.searchPageCtn}>
+        <View style={sty_SearchPage.topScreenCtn}>
+          <View style={{ flex: 1, backgroundColor: "#E83030" }}>
+            <View style={{ flex: 4 }}></View>
+            <View style={{ flex: 1, backgroundColor: "#5A5A5A" }}></View>
+          </View>
+          <ImageBackground
+            style={sty_SearchPage.topBackgroundImg}
+            source={img_topScrBackground}
+          >
+            <View style={{ flex: 1 }}></View>
+            <View style={{ flex: 2.5, flexDirection: "row" }}>
+              <View style={{ flex: 1 }}></View>
+              <View style={{ flex: 2, backgroundColor: "green" }}></View>
+              <View style={{ flex: 1 }}></View>
             </View>
-        )
-    }
+            <View style={{ flex: 1 }}></View>
+          </ImageBackground>
+        </View>
+        <View style={sty_SearchPage.bottomScreenCtn}>
+          <ImageBackground
+            style={sty_SearchPage.topBackgroundImg}
+            source={img_btmScrBackground}
+          ></ImageBackground>
+          <View style={{ flex: 1, backgroundColor: "#E83030" }}>
+            <View style={{ flex: 1, backgroundColor: "#5A5A5A" }}></View>
+            <View style={{ flex: 4 }}></View>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
--- RNProject/src/styles/SearchPageStyle.js
+++ RNProject/src/styles/SearchPageStyle.js
@@ -5,12 +5,15 @@ export default StyleSheet.create({
       backgroundColor: "blue",
       flex:1
     },
-
     topScreenCtn: {
         flex:1,
         backgroundColor:'red'
     },
-
+    topBackgroundImg: {
+        width: "100%",
+        height: undefined,
+        aspectRatio: 254 / 190,
+    },
     bottomScreenCtn: {
         flex:1,
         backgroundColor:'blue'
