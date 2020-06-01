2b3c9309d1883bae156aff6f48d26e89ac1af34e
John L <51428041+TooManyJohns@users.noreply.github.com>
2020-06-01 01:54:32 -0400

    Bottom of SearchPage, Numerical Button placed, layout framework  done

--- RNProject/src/components/SearchPage.js
+++ RNProject/src/components/SearchPage.js
@@ -1,42 +1,95 @@
 import React, { Component } from "react";
 
-import { View, ImageBackground } from "react-native";
+import { View, ImageBackground, TouchableOpacity } from "react-native";
 
 import sty_SearchPage from "styles/SearchPageStyle";
 
 import img_topScrBackground from "assets/search/topScrSearch.png";
 import img_btmScrBackground from "assets/search/btmScrSearch.png";
 
+import NumericalTxt from "assets/search/Numerical.png";
+
 export default class SearchPage extends Component {
   render() {
     return (
       <View style={sty_SearchPage.searchPageCtn}>
         <View style={sty_SearchPage.topScreenCtn}>
-          <View style={{ flex: 1, backgroundColor: "#E83030" }}>
-            <View style={{ flex: 4 }}></View>
-            <View style={{ flex: 1, backgroundColor: "#5A5A5A" }}></View>
+          <View style={sty_SearchPage.dexBorderRed}>
+            <View style={sty_SearchPage.borderSpacer}></View>
+            <View style={sty_SearchPage.dexBorderGrey}></View>
           </View>
           <ImageBackground
             style={sty_SearchPage.topBackgroundImg}
             source={img_topScrBackground}
           >
-            <View style={{ flex: 1 }}></View>
-            <View style={{ flex: 2.5, flexDirection: "row" }}>
-              <View style={{ flex: 1 }}></View>
-              <View style={{ flex: 2, backgroundColor: "green" }}></View>
-              <View style={{ flex: 1 }}></View>
+            <View style={sty_SearchPage.topScrTopProf}></View>
+            <View style={sty_SearchPage.topScrMiddleCtn}>
+              <View style={sty_SearchPage.topScrSideProf}></View>
+              <View style={sty_SearchPage.topScrAnimationCtn}></View>
+              <View style={sty_SearchPage.topScrSideProf}></View>
             </View>
-            <View style={{ flex: 1 }}></View>
+            <View style={sty_SearchPage.topScrDesc}></View>
           </ImageBackground>
         </View>
         <View style={sty_SearchPage.bottomScreenCtn}>
           <ImageBackground
             style={sty_SearchPage.topBackgroundImg}
             source={img_btmScrBackground}
-          ></ImageBackground>
-          <View style={{ flex: 1, backgroundColor: "#E83030" }}>
-            <View style={{ flex: 1, backgroundColor: "#5A5A5A" }}></View>
-            <View style={{ flex: 4 }}></View>
+          >
+            <View style={sty_SearchPage.orderCtn}>
+              <View style={sty_SearchPage.orderCtnSpL}></View>
+              <View style={sty_SearchPage.orderBtn}>
+                <TouchableOpacity style={sty_SearchPage.orderClickableCtn}>
+                  <View style={sty_SearchPage.genericBtnTopBtm}></View>
+                  <View style={sty_SearchPage.genericBtnMiddle}>
+                    <View style={sty_SearchPage.genericBtnSide}></View>
+                    <ImageBackground
+                      source={NumericalTxt}
+                      style={sty_SearchPage.sty_numericalTxt}
+                    ></ImageBackground>
+                    <View style={sty_SearchPage.genericBtnSide}></View>
+                  </View>
+                  <View style={sty_SearchPage.genericBtnTopBtm}></View>
+                </TouchableOpacity>
+              </View>
+              <View style={sty_SearchPage.orderCtnSpR}></View>
+            </View>
+            <View style={sty_SearchPage.nametypehtformCtn}>
+              <View style={sty_SearchPage.nametypehtCtn}>
+                <View style={sty_SearchPage.nameCtn}></View>
+                <View style={sty_SearchPage.typeCtn}></View>
+                <View style={sty_SearchPage.htCtn}></View>
+              </View>
+              <View style={sty_SearchPage.formCtn}></View>
+            </View>
+            <View style={sty_SearchPage.wtareaCtn}>
+              <View style={sty_SearchPage.wtCtn}></View>
+              <View style={sty_SearchPage.areaCtn}></View>
+              <View style={sty_SearchPage.wtareaFooter}></View>
+            </View>
+            <View style={sty_SearchPage.resetstartcancelCtn}>
+              <View style={sty_SearchPage.btnCtn}>
+                <View style={sty_SearchPage.tbBtnSpacer}></View>
+                <View style={sty_SearchPage.btn}></View>
+                <View style={sty_SearchPage.tbBtnSpacer}></View>
+              </View>
+              <View style={sty_SearchPage.btnCtnSpacer}></View>
+              <View style={sty_SearchPage.btnCtn}>
+                <View style={sty_SearchPage.tbBtnSpacer}></View>
+                <View style={sty_SearchPage.btn}></View>
+                <View style={sty_SearchPage.tbBtnSpacer}></View>
+              </View>
+              <View style={sty_SearchPage.btnCtnSpacer}></View>
+              <View style={sty_SearchPage.btnCtn}>
+                <View style={sty_SearchPage.tbBtnSpacer}></View>
+                <View style={sty_SearchPage.btn}></View>
+                <View style={sty_SearchPage.tbBtnSpacer}></View>
+              </View>
+            </View>
+          </ImageBackground>
+          <View style={sty_SearchPage.dexBorderRed}>
+            <View style={sty_SearchPage.dexBorderGrey}></View>
+            <View style={sty_SearchPage.borderSpacer}></View>
           </View>
         </View>
       </View>
--- RNProject/src/styles/SearchPageStyle.js
+++ RNProject/src/styles/SearchPageStyle.js
@@ -1,22 +1,128 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    searchPageCtn: {
-      backgroundColor: "blue",
-      flex:1
-    },
-    topScreenCtn: {
-        flex:1,
-        backgroundColor:'red'
-    },
-    topBackgroundImg: {
-        width: "100%",
-        height: undefined,
-        aspectRatio: 254 / 190,
-    },
-    bottomScreenCtn: {
-        flex:1,
-        backgroundColor:'blue'
-    }
-  });
-  
+  searchPageCtn: {
+    backgroundColor: "blue",
+    flex: 1,
+  },
+  topScreenCtn: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+  topScrTopProf: {
+    flex: 1,
+  },
+  topScrDesc: {
+    flex: 1,
+  },
+  topScrMiddleCtn: {
+    flex: 2.5,
+    flexDirection: "row",
+  },
+  topScrSideProf: {
+    flex: 1,
+  },
+  topScrAnimationCtn: {
+    flex: 2,
+    backgroundColor: "green",
+  },
+  topBackgroundImg: {
+    width: "100%",
+    height: undefined,
+    aspectRatio: 254 / 190,
+  },
+  bottomScreenCtn: {
+    flex: 1,
+  },
+  orderCtn: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  orderCtnSpL: {
+    flex: 2.9,
+  },
+  orderBtn: {
+    flex: 2.1,
+  },
+  orderCtnSpR: {
+    flex: 0.85,
+  },
+  orderClickableCtn: {
+    flex: 1,
+  },
+  genericBtnMiddle: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  genericBtnTopBtm: {
+    flex: 1,
+  },
+  genericBtnSide: {
+    flex: 0.5,
+  },
+  nametypehtformCtn: {
+    flex: 3.3,
+    backgroundColor: null,
+    flexDirection: "row",
+  },
+  nametypehtCtn: {
+    flex: 3,
+  },
+  nameCtn: {
+    flex: 1,
+  },
+  typeCtn: {
+    flex: 1,
+  },
+  htCtn: {
+    flex: 1,
+  },
+  formCtn: {
+    flex: 1,
+  },
+  wtareaCtn: {
+    flex: 2.7,
+  },
+  wtCtn: {
+    flex: 2,
+  },
+  areaCtn: {
+    flex: 2,
+  },
+  wtareaFooter: {
+    flex: 1,
+  },
+  resetstartcancelCtn: {
+    flex: 1.5,
+    flexDirection: "row",
+  },
+  btnCtn: {
+    flex: 1,
+  },
+  btnCtnSpacer: {
+    flex: 0.2,
+  },
+  tbBtnSpacer: {
+    flex: 1,
+  },
+  btn: {
+    flex: 3,
+  },
+  dexBorderRed: {
+    flex: 1,
+    backgroundColor: "#E83030",
+  },
+  dexBorderGrey: {
+    flex: 1,
+    backgroundColor: "#5A5A5A",
+  },
+  borderSpacer: {
+    flex: 4,
+  },
+  sty_numericalTxt: {
+    height: undefined,
+    width: "100%",
+    aspectRatio: 57 / 11,
+    flex: 1,
+  },
+});
