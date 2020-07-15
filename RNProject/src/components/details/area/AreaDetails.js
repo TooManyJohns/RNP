a6b2c83eb109f666c9e4a3ed55b01d6cc2388e72
Johnvinder <51428041+Johnvinder@users.noreply.github.com>
2020-07-15 03:04:51 -0400

    Almost finished styling with top screen, currently halfway on bottom screen, going through styling of the routeSelection screen in AreaDetails

--- RNProject/src/components/details/area/AreaDetails.js
+++ RNProject/src/components/details/area/AreaDetails.js
@@ -6,49 +6,103 @@ import BottomBorder from "components/borders/PokedexBottomBorder";
 import img_topScr from "assets/details/area/topDetailsArea.png";
 import img_btmScr from "assets/details/area/btmDetailsArea.png";
 
-import sty_AreaDetails from "styles/AreaDetailsStyle"
+import img_TESTSPRITE from "assets/details/area/TESTSPRITE.png";
 
-import { View, ImageBackground } from "react-native";
+import sty_AreaDetails from "styles/AreaDetailsStyle";
+
+import {
+  View,
+  ImageBackground,
+  Text,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
 export default class HomePage extends Component {
+  timeOfDayClicked = (timeOfDay) => {
+    switch (timeOfDay) {
+      case 1:
+        console.log("Morning");
+        break;
+      case 2:
+        console.log("Day");
+        break;
+      case 3:
+        console.log("Night");
+        break;
+    }
+  };
+
   render() {
     return (
-      <View style={{flex:1}}>
-          <TopBorder></TopBorder>
-            <ImageBackground 
-            style=
-            {{        
-                height: undefined,
-                width: "100%",
-                aspectRatio: 256 / 192,
-                flexDirection:'row'
-            }} 
-            source={img_topScr}
-            >
-              <View //LEFT SIDE TOP CTN
-              style={{flex:2, backgroundColor:''}}>
-                <View style={sty_AreaDetails.areaTopLeftCtn}></View>
-                <View style={{flex:0.3, backgroundColor:'red'}}></View>
-                <View style={{flex:0.6, backgroundColor:''}}></View>
-                <View style={{flex:1.1, backgroundColor:'yellow'}}></View>
+      <View style={{ flex: 1 }}>
+        <TopBorder></TopBorder>
+        <ImageBackground style={sty_AreaDetails.topScrCtn} source={img_topScr}>
+          <View //LEFT SIDE TOP CTN
+            style={sty_AreaDetails.topScreenLeftCtn}
+          >
+            <View style={sty_AreaDetails.areaLogoTopLeftCtn}></View>
+            <View style={sty_AreaDetails.timeOfDayCtn}>
+              <View style={sty_AreaDetails.timeOfDayCtnBtmTop}></View>
+              <Text style={sty_AreaDetails.timeOfDayFnt}>Morning</Text>
+              <View style={sty_AreaDetails.timeOfDayCtnBtmTop}></View>
+            </View>
+            <View style={sty_AreaDetails.topLeftCtnSpacer}></View>
+            <View style={sty_AreaDetails.topLeftCtnBottomCtn}>
+              <View style={sty_AreaDetails.topLeftCtnPokemonCtn}>
+                <View style={sty_AreaDetails.pokemonSprCtn}>
+                  <View style={sty_AreaDetails.pokemonSprTopSpacer}></View>
+                  <Image
+                    style={sty_AreaDetails.pokemonSpr}
+                    source={img_TESTSPRITE}
+                  ></Image>
+                </View>
+                <View style={sty_AreaDetails.pokemonNameCtn}>
+                  <Text //take in the pokemonselected props from PokemonList.js
+                    style={sty_AreaDetails.pokemonNameFnt}
+                  >
+                    BULBASAUR
+                  </Text>
+                </View>
               </View>
+              <View style={sty_AreaDetails.topLeftCtnBottomCtnBottomCtn}></View>
+            </View>
+          </View>
 
-              <View  //RIGHT SIDE TOP CTN
-              style={{flex:5.4, backgroundColor:'blue'}}></View>
-            </ImageBackground>
+          <View //RIGHT SIDE TOP CTN
+            style={{ flex: 5.4, backgroundColor: "blue" }}
+          ></View>
+        </ImageBackground>
 
-
-
-            <ImageBackground 
-            style=
-            {{        
-                height: undefined,
-                width: "100%",
-                aspectRatio: 256 / 192,
-            }} 
-            source={img_btmScr}
-            ></ImageBackground>
-          <BottomBorder></BottomBorder>
+        <ImageBackground //BOTTOM SCREEN
+          style={sty_AreaDetails.bottomScrCtn}
+          source={img_btmScr}
+        >
+          <View style={sty_AreaDetails.regionBtnCtn}></View>
+          <View style={sty_AreaDetails.timeOfDaySwitcherNRouteSelectionCtn}>
+            <View style={sty_AreaDetails.timeOfDaySwitcherCtn}>
+              <View style={sty_AreaDetails.timeOfDaySwitcherCtnTop}></View>
+              <TouchableOpacity
+                onPress={() => this.timeOfDayClicked(1)}
+                style={{ flex: 1, backgroundColor: "yellow" }}
+              ></TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => this.timeOfDayClicked(2)}
+                style={{ flex: 1, backgroundColor: "orange" }}
+              ></TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => this.timeOfDayClicked(3)}
+                style={{ flex: 1, backgroundColor: "darkblue" }}
+              ></TouchableOpacity>
+              <View style={sty_AreaDetails.timeOfDaySwitcherCtnBtm}></View>
+            </View>
+            <View style={sty_AreaDetails.routeSelectionCtn}>
+              <View style={sty_AreaDetails.routeSelection}></View>
+              <View style={sty_AreaDetails.routeSelectionCtnScroller}></View>
+            </View>
+          </View>
+        </ImageBackground>
+        <BottomBorder></BottomBorder>
       </View>
     );
   }
--- RNProject/src/styles/AreaDetailsStyle.js
+++ RNProject/src/styles/AreaDetailsStyle.js
@@ -1,7 +1,107 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-    areaTopLeftCtn: {
+    //TOP SCREEN
+    topScrCtn: {
+        height: undefined,
+        width: "100%",
+        aspectRatio: 256 / 192,
+        flexDirection:'row',
+    },
+    topScreenLeftCtn: {
+        flex:2
+    },
+    areaLogoTopLeftCtn: {
         flex:0.2, 
     },
+    timeOfDayCtn: {
+        flex:0.3
+    },
+    timeOfDayCtnBtmTop: {
+        flex:1
+    },
+    timeOfDayFnt: {
+        color: "#5A5A5A",
+        paddingLeft: 10,
+        paddingRight: 10,
+        fontSize: 24,
+        fontFamily: "Pokmon-DS-Font",
+        alignSelf: "center"
+    },
+    topLeftCtnSpacer: {
+        flex:0.6
+    },
+    topLeftCtnBottomCtn:{
+        flex:1.1,
+    },
+    topLeftCtnPokemonCtn:{
+        flex:1
+    },
+    topLeftCtnBottomCtnBottomCtn:{
+        flex:0.9,
+    },
+    pokemonNameCtn: {
+        flex:1,
+        alignItems:'center'
+    },
+    pokemonNameFnt: {
+        color: "#5A5A5A",
+        paddingLeft: 10,
+        paddingRight: 10,
+        fontSize: 24,
+        fontFamily: "Pokmon-DS-Font",
+        alignSelf: "center"
+    },
+    pokemonSprCtn: {
+        flex:1.8,
+        alignItems:'center'
+    },
+    pokemonSprTopSpacer: {
+        flex:0.15
+    },
+    pokemonSpr: {
+        flex:1
+    },
+
+    //BOTTOM SCREEN
+    bottomScrCtn: {
+        height: undefined,
+        width: "100%",
+        aspectRatio: 256 / 192,
+    },
+    regionBtnCtn: {
+        flex:1
+    },
+    timeOfDaySwitcherNRouteSelectionCtn:{
+        flex:6,
+        flexDirection:'row'
+    },
+    timeOfDaySwitcherCtn: {
+        flex:2
+    },
+    timeOfDaySwitcherCtnTop: {
+        flex:1
+    },
+    timeOfDaySwitcherCtnBtm: {
+        flex:1.2
+    },
+    routeSelectionCtn: {
+        flex:5,
+        flexDirection:'row'
+    },
+    routeSelectionCtn: {
+        flex:5,
+        flexDirection:'row'
+    },
+    routeSelection: {
+        flex:10
+    },
+    routeSelectionCtnScroller: {
+        flex:2
+    },
+
+
+    //front of bottom screen
+    
+
 })
